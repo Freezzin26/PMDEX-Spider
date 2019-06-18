06d61a7b6cc3a0b089647115d80b15d271e7fb16
Freezzin26 <1015882450@qq.com>
2019-06-18 22:56:41 +0800

    Update pipelines.py

--- myspider/pipelines.py
+++ myspider/pipelines.py
@@ -8,10 +8,5 @@ import logging
 
 class MyspiderPipeline(object):
     def process_item(self, item, spider):
-        with open('PMDEX.txt', 'a', encoding='utf-8') as f:
-            nlist = []
-            for i in item['pm_item']:
-                nlist.append(i[0])
-            f.write(' '.join(nlist) + '\n')
-            print(" ".join(nlist))
+        
         return item
